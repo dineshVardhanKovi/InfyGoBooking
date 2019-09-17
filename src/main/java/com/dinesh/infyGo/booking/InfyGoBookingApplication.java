c22f03a8b9229daaab814814582df7b9f99c1cd2
dkovi <dkovi@apple.com>
2019-09-17 14:51:41 +0530

    project created

---
+++ src/main/java/com/dinesh/infyGo/booking/InfyGoBookingApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.infyGo.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyGoBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGoBookingApplication.class, args);
+	}
+
+}
